030d229539b6a2c8ce073ac8e36be870bf8e78f7
MadinaAlili <119100084+MadinaAlili@users.noreply.github.com>
2024-12-22 01:21:11 +0400

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <header className="home-container">
+      <h1>Recipe Manager</h1>
+      <h3>ABC</h3>
+    </header>
+  );
+};
+
+export default Home;
